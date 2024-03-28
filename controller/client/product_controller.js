75b5c78da5e8c002699ddef336ca25c9ac55e666
Linh09 <hoangvlinh2kk4@gmail.com>
2024-03-29 02:34:15 +0700

    Làm trang danh sách bên phía Clients

--- controller/client/product_controller.js
+++ controller/client/product_controller.js
@@ -1,4 +1,18 @@
-//[GET] / product   
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index.pug");
+//[GET] / product 
+const Product = require("../../models/product.model.js");//Nhung file product.model.js
+
+module.exports.index = async (req, res) => {
+    //Viết logic để vào model lấy database
+    const products = await Product.find({
+        status : "active",//tim cac ban ghi co cac truong nhu dong 6 va 7
+        deleted : false
+    });// laay db bang ham find
+    for(const item of products) {
+        item.newPrice = (item.price * (100 - item.discountPercentage) /100).toFixed();
+    }
+
+    res.render("client/pages/products/index.pug", {
+        pageTitle : "Danh sách sản phẩm",
+        products : products//tra ra ngoai giao dien biến products chứa kiểu dữ liệu là mảng products
+    });
 }
