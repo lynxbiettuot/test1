335a07430ebc5f799d0abed27b5acec4b3b91832
Linh09 <hoangvlinh2kk4@gmail.com>
2024-04-17 12:38:47 +0700

    Xaty dung nhom quyen

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const dashBoardRoute = require("./dashboard.route.js");//Nhúng file home_route 
 const productRoute = require("./product.route.js");
 const productCateGoryRoutes = require("./product-category.route.js");
 const systemConfig = require("../../config/system");// Lấy ra biến trong cònfig
+const roleRoutes = require("./role.route.js");
 
 module.exports = (app) => {
     const path_admin = `/${systemConfig.prefixAdmin}`;
@@ -10,4 +11,6 @@ module.exports = (app) => {
     app.use(path_admin + "/products", productRoute);
 
     app.use(path_admin + "/products-category", productCateGoryRoutes);
+
+    app.use(path_admin + "/roles", roleRoutes);
 }
